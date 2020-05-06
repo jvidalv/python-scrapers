d445d36feb85c983115888394516ce164c7007d1
Josep Vidal <josepvidalvidal@gmail.com>
2020-05-06 22:47:28 +0200

    🦠 Spanish data is done, thanks SEMANA for your magnificient blog

--- main.py
+++ main.py
@@ -1,18 +1,62 @@
+import requests
 from pprint import pprint
-from pymongo import MongoClient
+from bs4 import BeautifulSoup
 from src.utils.Mongo import Mongo
 from src.keys.db_mongo import mongo_connection
+from datetime import datetime
+from src.constants.signs import *
 
 mongo = Mongo(mongo_connection)
-serverStatusResult = mongo.db.users.find()
+arrayDailyData = []
 
-pprint(serverStatusResult)
-# URL = 'https://www.hola.com/horoscopo/'
-# page = requests.get(URL)
-#
-# soup = BeautifulSoup(page.content, "html.parser")
-#
-# titles = soup.select("h2.titulo")
-#
-# for row in titles:
-#     print(row.get_text().strip())
+# DAY ---
+day = datetime.today().strftime('%Y-%m-%d')
+
+# SIGNS BASE ---
+for sign in signs_en:
+    arrayDailyData.append({
+        'day': day,
+        'sign': sign,
+        'contents': {
+            'love':
+                {'es': '', 'en': ''},
+            'health':
+                {'es': '', 'en': ''},
+            'work':
+                {'es': '', 'en': ''},
+            'you_hate': [],
+            'you_love': [],
+            'numbers': []
+        }
+    })
+
+# SPANISH
+# Get the latest blog entry for this blog ( 1 each day )
+spanishBaseUrl = 'https://www.semana.es/horoscopo/'
+page = requests.get(spanishBaseUrl)
+spanishSoup = BeautifulSoup(page.content, "html.parser")
+days = spanishSoup.select("div.td_module_10:first-child a:first-child")
+dayUrl = days[0].get('href')
+
+spanishDayUrl = dayUrl
+page = requests.get(spanishDayUrl)
+spanishSoup = BeautifulSoup(page.content, "html.parser")
+spanishSigns = spanishSoup.select("div.td-post-content h3")
+
+# We get love, work and health texts for spanish here
+for dailyData in arrayDailyData:
+    spSign = signs_en_to_es[dailyData['sign']]
+    for pageSign in spanishSigns:
+        if pageSign.text == spSign:
+            sections = pageSign.parent.select('p > strong')
+            dailyData['contents']['health']['es'] = pageSign.find_next_sibling('p').text.replace('Salud:', '').strip()
+            dailyData['contents']['love']['es'] = pageSign.find_next_sibling('p').find_next_sibling('p').text.replace(
+                'Amor:', '').strip()
+            dailyData['contents']['work']['es'] = pageSign.find_next_sibling('p').find_next_sibling(
+                'p').find_next_sibling('p').text \
+                .replace('Ver más sobre ' + spSign + '…', '') \
+                .replace('Dinero y trabajo:', '') \
+                .strip()
+
+# ENGLISH
+# @todo
---
+++ src/constants/signs.py
@@ -0,0 +1,45 @@
+signs_es = [
+    "Acuario",
+    "Aries",
+    "Cáncer",
+    "Capricornio",
+    "Géminis",
+    "Leo",
+    "Libra",
+    "Piscis",
+    "Sagitario",
+    "Escorpio",
+    "Tauro",
+    "Virgo",
+]
+
+signs_en_to_es = {
+    'Capricorn': 'Capricornio',
+    'Aquarius': 'Acuario',
+    'Pisces': 'Piscis',
+    'Aries': 'Aries',
+    'Taurus': 'Tauro',
+    'Gemini': 'Géminis',
+    'Cancer': 'Cáncer',
+    'Leo': 'Leo',
+    'Virgo': 'Virgo',
+    'Libra': 'Libra',
+    'Scorpio': 'Escorpio',
+    'Sagittarius': 'Sagitario',
+
+}
+
+signs_en = [
+    'Capricorn',
+    'Aquarius',
+    'Pisces',
+    'Aries',
+    'Taurus',
+    'Gemini',
+    'Cancer',
+    'Leo',
+    'Virgo',
+    'Libra',
+    'Scorpio',
+    'Sagittarius',
+]
